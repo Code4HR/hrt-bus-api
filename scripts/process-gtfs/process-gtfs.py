37c7b6b3bc8473dcf77f48d44ce295ca065ddef9
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2013-01-06 14:05:48 -0500

    Read active trips and stop times into memory

--- scripts/process-gtfs/process-gtfs.py
+++ scripts/process-gtfs/process-gtfs.py
@@ -2,15 +2,39 @@ import json
 from StringIO import StringIO
 from urllib import urlopen
 from zipfile import ZipFile
+from csv import DictReader
+from datetime import datetime, timedelta
 
 feedUrl = "http://www.gtfs-data-exchange.com/api/agency?agency=hampton-roads-transit-hrt"
 fileUrl = json.loads(urlopen(feedUrl).read())['data']['datafiles'][0]['file_url']
 zipFile = ZipFile(StringIO(urlopen(fileUrl).read()))
 
-calLines = zipFile.open("calendar.txt").read().splitlines()
-for line in calLines:
-	print line
+days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+curDate = (datetime.utcnow() + timedelta(hours=-5)).date()
+curWeekDay = days[curDate.weekday()]
+print curWeekDay + " " + str(curDate)
 
-stopTimesLines = zipFile.open("stop_times.txt").read().splitlines()
-for idx, line in enumerate(stopTimesLines):
-    print idx, line
+activeServiceIds = []
+calendar = DictReader(zipFile.open("calendar.txt"))
+for row in calendar:
+	start = datetime.strptime(row['start_date'], "%Y%m%d").date()
+	end = datetime.strptime(row['end_date'], "%Y%m%d").date()
+	if curDate >= start and curDate <= end and row[curWeekDay] == '1':
+		activeServiceIds.append(row['service_id'])
+print activeServiceIds
+
+activeTrips = []
+activeTripIds = []
+trips = DictReader(zipFile.open("trips.txt"))
+for row in trips:
+	if row['service_id'] in activeServiceIds:
+		activeTrips.append(row)
+		activeTripIds.append(row['trip_id'])
+print str(len(activeTrips)) + " active trips"
+
+activeStopTimes = []
+stopTimes = DictReader(zipFile.open("stop_times.txt"))
+for row in stopTimes:
+	if row['trip_id'] in activeTripIds:
+		activeStopTimes.append(row)
+print str(len(activeStopTimes)) + " active stop times"
