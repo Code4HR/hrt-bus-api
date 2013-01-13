6ba6b1e1132cd645e1c3692c4592e2f695db4cd9
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2013-01-13 15:24:04 -0500

    Missed a field in the last commit

--- scripts/process-ftp/process-ftp.py
+++ scripts/process-ftp/process-ftp.py
@@ -30,7 +30,7 @@ def process(text):
 	
 	if hasattr(checkin, 'routeId'):
 		checkin.tripId = db.getTripId(checkin)
-		busRouteMappings[checkin.busId] = {'routeId' : checkin.routeId, 'tripId': checkin.tripId, 'time': checkin.time}
+		busRouteMappings[checkin.busId] = {'busId': checkin.busId, 'routeId' : checkin.routeId, 'tripId': checkin.tripId, 'time': checkin.time}
 		stats['hadRoute'] += 1
 	elif checkin.busId in busRouteMappings:
 		checkin.routeId = busRouteMappings[checkin.busId]['routeId']
