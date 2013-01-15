34d106862eb255196cbb01990f75b05aa240bde6
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2013-01-14 23:38:35 -0500

    Added web api to get active routes

--- web/app.py
+++ web/app.py
@@ -1,11 +1,20 @@
 import os
 from flask import Flask
+from pymongo import Connection
 
 app = Flask(__name__)
+db = Connection(os.environ['MONGO_URI']).hrt
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/api/routes/')
+def getActiveRoutes():
+	routes = db['checkins'].distinct('routeId')
+	routes.sort()
+	return str(routes)
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
