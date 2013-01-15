544fed27a8ed5cd303ca741399149825688d9acb
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2013-01-15 00:09:41 -0500

    Added a couple more APIs to the web app

--- web/app.py
+++ web/app.py
@@ -1,9 +1,9 @@
-import os
 from flask import Flask
-from pymongo import Connection
+import os
+import pymongo
 
 app = Flask(__name__)
-db = Connection(os.environ['MONGO_URI']).hrt
+db = pymongo.Connection(os.environ['MONGO_URI']).hrt
 
 @app.route('/')
 def hello():
@@ -15,8 +15,17 @@ def getActiveRoutes():
 	routes.sort()
 	return str(routes)
 
+@app.route('/api/route/<int:routeId>/')
+def getBusesOnRoute(routeId):
+	buses = db['checkins'].find({"routeId":routeId}).distinct('busId')
+	buses.sort()
+	return str(buses)
+	
+@app.route('/api/bus/<int:busId>/')
+def getBusHistory(busId):
+	return str(list(db['checkins'].find({"busId":busId}).sort('time', pymongo.DESCENDING)))
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
