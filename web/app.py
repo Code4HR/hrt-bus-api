7edfb21060db225f67a665c2884960519e077bad
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2013-01-15 00:44:17 -0500

    Added API to find closest stop

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from geopy import geocoders
 import os
 import pymongo
 
@@ -24,6 +25,12 @@ def getBusesOnRoute(routeId):
 @app.route('/api/bus/<int:busId>/')
 def getBusHistory(busId):
 	return str(list(db['checkins'].find({"busId":busId}).sort('time', pymongo.DESCENDING)))
+
+@app.route('/api/stop/<city>/<intersection>/')
+def getNearestStop(city, intersection):
+	geocoders.Google()
+	place, (lat, lng) = geocoders.Google().geocode("{0}, {1}, VA".format(intersection, city))
+	return str(db['stops'].find_one({"location": {"$near": [lng, lat]}}))
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
