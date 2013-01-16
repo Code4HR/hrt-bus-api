16183039ee4d5fa88638abdedcbeba3e7d3b0497
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2013-01-16 07:54:06 -0500

    First shot at next bus API
    Returns all scheduled stops for the day with adherence added where
    possible.

--- web/app.py
+++ web/app.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from flask import Flask
 from geopy import geocoders
 import os
@@ -18,9 +19,10 @@ def getActiveRoutes():
 
 @app.route('/api/route/<int:routeId>/')
 def getBusesOnRoute(routeId):
-	buses = db['checkins'].find({"routeId":routeId}).distinct('busId')
-	buses.sort()
-	return str(buses)
+	checkins = {}
+	for checkin in db['checkins'].find({"routeId":routeId}).sort('time'):
+		checkins[checkin['busId']] = checkin
+	return str(checkins)
 	
 @app.route('/api/bus/<int:busId>/')
 def getBusHistory(busId):
@@ -31,6 +33,17 @@ def getNearestStop(city, intersection):
 	geocoders.Google()
 	place, (lat, lng) = geocoders.Google().geocode("{0}, {1}, VA".format(intersection, city))
 	return str(db['stops'].find_one({"location": {"$near": [lng, lat]}}))
+
+@app.route('/api/nextbus/<int:routeId>/<int:stopId>/')
+def getNextBus(routeId, stopId):
+	time = datetime.utcnow()
+	collectionName = 'gtfs_' + (time + timedelta(hours=-5)).strftime('%Y%m%d')
+	scheduledStops = list(db[collectionName].find({"route_id":routeId, "stop_id":stopId}).sort('arrival_time'))
+	for stop in scheduledStops:
+		busOnTrip = db['checkins'].find_one({"tripId":stop["trip_id"]})
+		if busOnTrip is not None:
+			stop['adherence'] = busOnTrip['adherence']
+	return str(scheduledStops)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
