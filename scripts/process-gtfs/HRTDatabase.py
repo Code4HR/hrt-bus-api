162400e59a413403fe3069a481cc8a65cd352acf
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2013-08-21 11:56:48 -0400

    Tabs to Spaces

--- scripts/process-gtfs/HRTDatabase.py
+++ scripts/process-gtfs/HRTDatabase.py
@@ -2,35 +2,35 @@ from datetime import datetime, timedelta
 from pymongo import Connection, GEO2D
 
 class HRTDatabase:
-	def __init__(self, uri, db):
-		self.client = Connection(uri)
-		self.database = self.client[db]
+    def __init__(self, uri, db):
+        self.client = Connection(uri)
+        self.database = self.client[db]
 
-	def removeOldGTFS(self, date):
-		print "Removing Old GTFS Data"
-		collectionPrefix = self.genCollectionName('', date)
-		for collection in self.database.collection_names():
-			if collection.find('_') != -1 and (not collection.endswith(collectionPrefix)):
-				self.database.drop_collection(collection)
-	
-	def insertGTFS(self, data, date):
-	    collectionName = self.genCollectionName('gtfs_', date)
-		self.insertData(collectionName, data)
-		self.database[collectionName].ensure_index('block_id')
-	
-	def insertStops(self, data, date):
-		collectionName = self.genCollectionName('stops_', date)
-		self.insertData(collectionName, data)
-		self.database[collectionName].ensure_index( [('location', GEO2D)] )
-	
-	def insertRoutes(self, data, date):
-		self.insertData(self.genCollectionName('routes_', date), data)
-	
-	def genCollectionName(self, prefix, date):
-		return prefix + date.strftime('%Y%m%d')
-	
-	def insertData(self, collectionName, data):
-		if len(data) > 0:
-			self.database[collectionName].remove()
-			self.database[collectionName].insert(data)
-	
+    def removeOldGTFS(self, date):
+        print "Removing Old GTFS Data"
+        collectionPrefix = self.genCollectionName('', date)
+        for collection in self.database.collection_names():
+            if collection.find('_') != -1 and (not collection.endswith(collectionPrefix)):
+                self.database.drop_collection(collection)
+    
+    def insertGTFS(self, data, date):
+        collectionName = self.genCollectionName('gtfs_', date)
+        self.insertData(collectionName, data)
+        self.database[collectionName].ensure_index('block_id')
+    
+    def insertStops(self, data, date):
+        collectionName = self.genCollectionName('stops_', date)
+        self.insertData(collectionName, data)
+        self.database[collectionName].ensure_index( [('location', GEO2D)] )
+    
+    def insertRoutes(self, data, date):
+        self.insertData(self.genCollectionName('routes_', date), data)
+    
+    def genCollectionName(self, prefix, date):
+        return prefix + date.strftime('%Y%m%d')
+    
+    def insertData(self, collectionName, data):
+        if len(data) > 0:
+            self.database[collectionName].remove()
+            self.database[collectionName].insert(data)
+    
