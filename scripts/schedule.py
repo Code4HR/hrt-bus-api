3ce4c744ffc5090913c72636b071fafbc987bf99
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2018-02-16 14:46:38 -0500

    Start FTP scheduler

--- scripts/schedule.py
+++ scripts/schedule.py
@@ -4,13 +4,15 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-#@sched.scheduled_job('interval', minutes=1)
-#def ftp_job():
-#    print 'Running FTP Job
-#    ftp.process({}, None)
-#    print 'FTP Job Complete'
+# Process FTP every minute
+@sched.scheduled_job('interval', minutes=1)
+def ftp_job():
+    print 'Running FTP Job
+    ftp.process({}, None)
+    print 'FTP Job Complete'
 
-@sched.scheduled_job('cron', hour=19, minute=30)
+# Each day at 3 AM, load schedules for the next day
+@sched.scheduled_job('cron', hour=3)
 def gtfs_job():
     print  'Running GTFS Job'
     gtfs.process({}, None)
